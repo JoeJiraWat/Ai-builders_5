97ed63b0c8028735de78fba9f2737580a2e4495c
JoeJiraWat <joejiraw@gmail.com>
2025-06-10 22:22:03 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,7 +5,7 @@ import soundfile as sf
 from transformers import AutoModelForSpeechSeq2Seq, AutoProcessor
 
 # โหลดโมเดลจาก Hugging Face
-MODEL_NAME = "B4by/Test"
+MODEL_NAME = "https://huggingface.co/B4by/Test"
 model = AutoModelForSpeechSeq2Seq.from_pretrained(MODEL_NAME)
 processor = AutoProcessor.from_pretrained(MODEL_NAME)
 
